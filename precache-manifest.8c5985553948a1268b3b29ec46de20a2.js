045364fc8de7cd6da2eb98d3d2f208ca45ca0aa6
anjalipal2001 <ap7197339@gmail.com>
2021-04-20 15:58:53 +0530

    Updates

---
+++ precache-manifest.8c5985553948a1268b3b29ec46de20a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09b2054ba0b31d90dc599fa7e8ce075b",
+    "url": "/newsreader/index.html"
+  },
+  {
+    "revision": "39c97093c1bab588b8fa",
+    "url": "/newsreader/static/css/main.50e9c6da.chunk.css"
+  },
+  {
+    "revision": "7ccb9089acef15d5ec5f",
+    "url": "/newsreader/static/js/2.58a8787f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/newsreader/static/js/2.58a8787f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39c97093c1bab588b8fa",
+    "url": "/newsreader/static/js/main.62193480.chunk.js"
+  },
+  {
+    "revision": "749488af8dcec9472b2d",
+    "url": "/newsreader/static/js/runtime-main.edd78433.js"
+  }
+]);
